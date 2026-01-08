9f992280a73b6f0a75fd4d60ef00fe75ed16de40
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2026-01-08 08:17:59 +0500

    Update Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Hero from './component/Hero';
 import Service from './component/Services';
 import Portfolio from './component/Portfolio';
-import ContactCTA from './component/ContactCTA'
+import ContactCTA from './component/ContactCTA';
+import AboutSection from './component/AboutSection';
+import ContactSection from './component/ContactSection'
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <Service />
       <Portfolio />
       <ContactCTA />
+      <AboutSection />
+      <ContactSection />
     </>
   );
 }
